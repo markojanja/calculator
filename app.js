2c83c2dbe9d50143d3b10eb51305c2aac871915e
Marko Janjic <marko.m.janjic@gmail.com>
2022-08-15 11:31:04 +0200

    Improve display funcionality and added multply to the operators

--- app.js
+++ app.js
@@ -1,151 +1,158 @@
-"use strict"
+"use strict";
 
 // Math functions
 
-function add(x, y){
-    if(!y){
-        return x
-    }
-    return x+y
+function add(x, y) {
+  if (!y) {
+    return x;
+  }
+  return x + y;
 }
 
-function subtract(x, y){
-    if(!y){
-        return x
-    }
-    return x-y
+function subtract(x, y) {
+  if (!y) {
+    return x;
+  }
+  return x - y;
 }
 
-function divide(x, y){
-    if(!y){
-        return x
-    }
-    else if (x===0){
-        return "Error"
-    }
-    return x/y
+function divide(x, y) {
+  if (!y) {
+    return x;
+  } else if (x === 0) {
+    return "Error";
+  }
+  return x / y;
 }
 
-function multiply(x, y){
-    if(!y){
-        return x
-    }
-    return x*y
+function multiply(x, y) {
+  if (!y) {
+    return x;
+  }
+  return x * y;
+}
+
+function power(x, y) {
+  if (!y) {
+    return x;
+  }
+  return x ** y;
 }
 //operators object
 const operators = {
-    "+":add,
-    "-":subtract,
-    "/":divide,
-    "*":multiply,  
-}
-
+  "+": add,
+  "-": subtract,
+  "/": divide,
+  "*": multiply,
+  "^": power,
+};
 
 // operate numbers
-
-function operate(x, y, operator){
-    switch(operator){
-        case "+":
-            return operators[operator](x, y)
-        case "-":
-            return operators[operator](x, y)
-        case "/":
-            return operators[operator](x, y)
-        case "*":
-            return operators[operator](x, y)
-        default:
-            return x    
-    }
+function operate(x, y, operator) {
+  switch (operator) {
+    case "+":
+      return operators[operator](x, y);
+    case "-":
+      return operators[operator](x, y);
+    case "/":
+      return operators[operator](x, y);
+    case "*":
+      return operators[operator](x, y);
+    case "^":
+      return operators[operator](x, y);
+    default:
+      return x;
+  }
 }
-//update display 
+//update display
 
-function updateDisplay(){
-    if(result === null){
-        return display.textContent = "0";
-    }
-    return display.textContent = `${result}`
+function updateDisplay() {
+  if (result === null) {
+    return (display.textContent = "0");
+  }
+  return (display.textContent = `${result}`);
 }
 
+const numberBtns = document.querySelectorAll(".number-btn");
+const operatorBtns = document.querySelectorAll(".operator-btn");
+const display = document.querySelector(".display");
 
-const numberBtns = document.querySelectorAll('.number-btn');
-const operatorBtns = document.querySelectorAll('.operator-btn');
-const display = document.querySelector('.display');
-
-
-let userInput =  "";
+let userInput = "";
 let previousNumber;
-let secondNumber;
 let currentNumber;
 let operator;
-let result=null;
+let result = null;
 
-function clearAll(){
-    userInput ="";
-    previousNumber;
-    secondNumber;
-    currentNumber=null;
-    operator = undefined;
-    result = null;
-    numberBtns.forEach(n=> n.disabled = false)   
+function clearAll() {
+  userInput = "";
+  previousNumber;
+  currentNumber = null;
+  operator = undefined;
+  result = null;
+  numberBtns.forEach((n) => (n.disabled = false));
 }
 
-updateDisplay()
-clearAll()
-numberBtns.forEach(number=>{
-    number.addEventListener('click',(e)=>{
-        e.preventDefault()
-        userInput+= e.target.innerText;
-        if(operator === "="){
-           userInput =""
-           userInput+= e.target.innerText;
-           previousNumber = parseFloat(userInput)
-        }
-        if(number.innerText === "." && userInput.includes(".")){
-            number.disabled = true;
-        }
-        
-        currentNumber = parseFloat(userInput);
-        if(currentNumber !== undefined){
-            display.textContent = `${currentNumber}`
-        }
-        
-        if(operator!==undefined){
-            
-            currentNumber = parseFloat(userInput);
-          
-            result = operate(previousNumber, currentNumber, operator); 
-            console.log(currentNumber)   
-            currentNumber=result;
-            
-            
-        } 
-    })
-})
+updateDisplay();
+clearAll();
+numberBtns.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    e.preventDefault();
+    userInput += e.target.innerText;
 
-operatorBtns.forEach(op=>{
-    op.addEventListener('click', (e)=>{
-        operator = e.target.innerText;
-        if(operator!==undefined && currentNumber===null){
-            return display.textContent = "error";
-        }
-        previousNumber = currentNumber
-        userInput ="";
-        if(userInput === ""){
-            numberBtns.forEach(n=> n.disabled = false)
-        }
-        if(operator ==="="){
-            display.textContent = `${operator}${result}`
-        }
-        else
-        {display.textContent = `${previousNumber}${operator}`}
+    if(isNaN(userInput)){
+        number.disabled = true;
+        return
+    }
+    if (operator === "=") {
+      userInput = "";
+      userInput += e.target.innerText;
+      previousNumber = parseFloat(userInput);
+    }
+    if (number.innerText === "." && userInput.includes(".")) {
+      number.disabled = true;
+    }
+    currentNumber = parseFloat(userInput);
+    if (currentNumber !== undefined) {
+      display.textContent = `${currentNumber}`;
+    }
+    if (operator !== undefined) {
+      currentNumber = parseFloat(userInput);
+      result = operate(previousNumber, currentNumber, operator);
+      if (operator === "=") {
+        return (display.textContent = `${result}`);
+      }
+      display.textContent = `${previousNumber}${operator}${currentNumber}`;
+      currentNumber = result;
+    }
+  });
+});
 
-        
-    })
-})
+operatorBtns.forEach((op) => {
+  op.addEventListener("click", (e) => {
+    operator = e.target.innerText;
+    if (operator !== undefined && currentNumber === null) {
+      clearAll();
+      return (display.textContent = "error");
+    }
+    previousNumber = currentNumber;
+    userInput = "";
+    if (userInput === "") {
+      numberBtns.forEach((n) => (n.disabled = false));
+    }
+    if (operator === "=") {
+      if (result === null) {
+        return (display.textContent = "error");
+      }
+      display.textContent = `${operator} ${result}`;
+    } else {
+      display.textContent = `${previousNumber}${operator}`;
+    }
+  });
+});
 
-const clearBtn = document.querySelector('.clear-btn');
+const clearBtn = document.querySelector(".clear-btn");
 
-clearBtn.addEventListener('click', ()=>{
-    clearAll() 
-    updateDisplay()
-})
+clearBtn.addEventListener("click", () => {
+  clearAll();
+  updateDisplay();
+});
