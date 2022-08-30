edc94461dee0e7225d28c76facdb9807ecbdedf5
Marko Janjic <marko.m.janjic@gmail.com>
2022-08-30 22:19:28 +0200

    code refactoring

--- app.js
+++ app.js
@@ -3,32 +3,21 @@
 // Math functions
 
 function add(x, y) {
-  if (!y) {
-    return x;
-  }
   return x + y;
 }
 
 function subtract(x, y) {
-  if (!y) {
-    return x;
-  }
+
   return x - y;
 }
 
 function divide(x, y) {
-  if (!y) {
-    return x;
-  } else if (x === 0) {
-    return "Error";
-  }
+
   return x / y;
 }
 
 function multiply(x, y) {
-  if (!y) {
-    return x;
-  }
+
   return x * y;
 }
 
@@ -68,21 +57,10 @@ function operate(x, y, operator) {
 
 function updateDisplay() {
   if (result === null) {
-    return (display.textContent = "0");
+    return display.textContent = "0";
   }
-  return (display.textContent = `${result}`);
+  return display.textContent = `${result}`;
 }
-
-const numberBtns = document.querySelectorAll(".number-btn");
-const operatorBtns = document.querySelectorAll(".operator-btn");
-const display = document.querySelector(".display");
-
-let userInput = "";
-let previousNumber;
-let currentNumber;
-let operator;
-let result = null;
-
 function clearAll() {
   userInput = "";
   previousNumber;
@@ -92,32 +70,49 @@ function clearAll() {
   numberBtns.forEach((n) => (n.disabled = false));
 }
 
-updateDisplay();
-clearAll();
+const numberBtns = document.querySelectorAll(".number-btn");
+const operatorBtns = document.querySelectorAll(".operator-btn");
+const display = document.querySelector(".display");
+const clearBtn = document.querySelector(".clear-btn");
+
+let userInput = "";
+let previousNumber;
+let currentNumber;
+let operator;
+let result = null;
+
+
+
 numberBtns.forEach((number) => {
   number.addEventListener("click", (e) => {
     userInput += e.target.innerText;
     currentNumber = parseFloat(userInput);
+    if(isNaN(currentNumber)) return number.disabled = true
+
+    if (number.innerText === "." && userInput.includes(".")) {
+      number.disabled = true;
+    }
     if (operator === "=") {
       userInput = "";
       userInput += e.target.innerText;
       previousNumber = parseFloat(userInput);
     }
-    if (number.innerText === "." && userInput.includes(".")) {
-      number.disabled = true;
-    }
-
     if (currentNumber !== undefined) {
       display.textContent = `${currentNumber}`;
     }
     if (operator !== undefined) {
       currentNumber = parseFloat(userInput);
-      result = operate(currentNumber,previousNumber, operator);
-      if (operator === "=") {
-        return display.textContent = `${result}`;
-      }
+      result = operate(previousNumber,currentNumber, operator);
       display.textContent = `${previousNumber}${operator}${currentNumber}`;
       currentNumber = result;
+      if (operator === "=") {
+        clearAll()
+        userInput = ""
+        userInput += e.target.textContent;
+        currentNumber = parseFloat(userInput)
+        return display.textContent = `${currentNumber}`;
+
+      }
     }
   });
 });
@@ -125,27 +120,32 @@ numberBtns.forEach((number) => {
 operatorBtns.forEach((op) => {
   op.addEventListener("click", (e) => {
     operator = e.target.innerText;
+    previousNumber = currentNumber;
+    userInput = "";
     if (operator !== undefined && currentNumber === null) {
       clearAll();
       return display.textContent = "error";
     }
-    previousNumber = currentNumber;
-    userInput = "";
     if (userInput === "") {
       numberBtns.forEach((n) => (n.disabled = false));
     }
     if (operator === "=") {
       if (result === null) {
+        result = previousNumber
+        return display.textContent = `${previousNumber}`;
+      }
+      if(result===Infinity){
+        clearAll()
         return display.textContent = "error";
       }
+      console.log(previousNumber, currentNumber, result)
       display.textContent = `${operator} ${result}`;
-    } else {
+    } 
+    else {
       display.textContent = `${previousNumber}${operator}`;
     }
   });
 });
-
-const clearBtn = document.querySelector(".clear-btn");
 
 clearBtn.addEventListener("click", () => {
   clearAll();
